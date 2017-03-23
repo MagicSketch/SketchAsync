7268cbe668936cff72c314fc5a53f98a46047ca6
James Tang <j@jamztang.com>
2017-03-23 17:53:38 +0800

    Initial Commit

---
+++ SketchAsync/SketchAsync.h
@@ -0,0 +1,19 @@
+//
+//  SketchAsync.h
+//  SketchAsync
+//
+//  Created by James Tang on 23/3/2017.
+//  Copyright © 2017 MagicSketch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SketchAsync.
+FOUNDATION_EXPORT double SketchAsyncVersionNumber;
+
+//! Project version string for SketchAsync.
+FOUNDATION_EXPORT const unsigned char SketchAsyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SketchAsync/PublicHeader.h>
+
+
